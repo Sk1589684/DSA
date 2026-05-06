7d22eb87cfc537777f5bf70a941ac23b113ae7a0
Sarvesh Kulkarni 47 <sarveshkulkarni.2003@gmail.com>
2026-05-06 14:28:25 +0530

    Add files via upload

---
+++ com/demo/test/TestBubbleSort.java
@@ -0,0 +1,18 @@
+package com.demo.test;
+
+
+import java.util.stream.IntStream;
+
+import Sorting.BubbleSortAlgorithm;
+
+public class TestBubbleSort {
+
+	public static void main(String[] args) {
+		
+		int[] arr= {10,20,1,20,100,3,5,11,101,99,98};
+		
+//		BubbleSortAlgorithm.bubbleSortDescending(arr);
+		
+		BubbleSortAlgorithm.bubbleSortDescending(arr);
+	}
+}
